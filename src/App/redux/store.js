e0bf7cdc01e2bb0cc47f7db9622c680250147ae4
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2020-12-16 06:18:50 +0200

    predelpoy_without_toolkit

--- src/App/redux/store.js
+++ src/App/redux/store.js
@@ -1,11 +1,15 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 
 //reducers
 import reducer from "./reducer";
 
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+// const store = createStore(
+//   reducer,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+const store = configureStore({
+  reducer
+})
 
 export default store;
